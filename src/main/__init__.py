11032de5c3dfe317ab3bb232cfc5a294991741a2
Fluttrr <fluttr@proton.me>
2024-11-20 19:16:58 +0100

    Changed from "separator" to "tokenizer"

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -3,7 +3,7 @@
 
 # Import necessary modules from the package
 from .ocr import text_from_folder
-from .separator import vocab_from_texts
+from .tokenizer import vocab_from_texts
 from .main import main
 
 # Define what is available when the package is imported
--- src/main/main.py
+++ src/main/main.py
@@ -8,31 +8,43 @@ import logging
 
 # Local application imports
 from . import ocr
-from . import separator
+from . import tokenizer
 from . import csv
+
 
 def parse_arguments():
     """Parse command-line arguments."""
     import argparse
-    parser = argparse.ArgumentParser(description="This script allows you to extract a vocabulary list from a folder of manga images.")
-    parser.add_argument("--parent", action="store_true", help="Provided folder contains multiple volumes. Each folder will be treated as its own volume.")
+
+    parser = argparse.ArgumentParser(
+        description="This script allows you to extract a vocabulary list from a folder of manga images."
+    )
+    parser.add_argument(
+        "--parent",
+        action="store_true",
+        help="Provided folder contains multiple volumes. Each folder will be treated as its own volume.",
+    )
     parser.add_argument("folder", type=str, help="Path to the folder.")
     return parser.parse_args()
 
+
 def main():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     args = parse_arguments()
 
     provided_folder = Path(args.folder)
 
     texts = ocr.text_from_folder(provided_folder, args.parent)
-    logging.debug(f"Texts: {texts}") 
-    vocab = separator.vocab_from_texts(texts)
+    logging.debug(f"Texts: {texts}")
+    vocab = tokenizer.vocab_from_texts(texts)
     logging.info(f"Vocabulary: {vocab}")
 
     output_folder = provided_folder if args.parent else provided_folder.parent
     output_file = output_folder / "vocab.csv"
     csv.save_vocab_to_csv(vocab, output_file)
-            
+
+
 if __name__ == "__main__":
     main()
