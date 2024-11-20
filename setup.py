3afd3de75f6b8484427e3f63ff064b32171a1e5a
Fluttrr <fluttr@proton.me>
2024-11-20 14:46:52 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manga-wordlist-extractor',
-    version='0.1.2',
+    version='0.1.4',
     description='A utility to extract vocabulary lists from manga.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
