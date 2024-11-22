87b4e6d5b317bcfa93a2a7dab1d7f3ef8bfe6810
Fluttrr <fluttr@proton.me>
2024-11-22 10:11:41 +0100

    Add new functions to package exports in __init__.py

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -5,6 +5,19 @@
 from .ocr import text_from_folder
 from .tokenizer import vocab_from_texts
 from .main import main
+from .csv import save_vocab_to_csv, add_english_to_vocab
+from .pdf import text_from_pdf
+from .epub import texts_from_epub
+from .definition import lookup_definition
 
 # Define what is available when the package is imported
-__all__ = ["text_from_folder", "vocab_from_texts", "main"]
+__all__ = [
+    "text_from_folder",
+    "vocab_from_texts",
+    "main",
+    "save_vocab_to_csv",
+    "add_english_to_vocab",
+    "text_from_pdf",
+    "texts_from_epub",
+    "lookup_definition",
+]
