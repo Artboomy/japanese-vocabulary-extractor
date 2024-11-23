c5b2fe5bf614611b16cd6a7ce3c7a3a5dbb455e5
Fluttrr <fluttr@proton.me>
2024-11-23 18:24:52 +0100

    Bump version to 1.1.1 for japanese-vocabulary-extractor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="japanese-vocabulary-extractor",
-    version="1.1.0",
+    version="1.1.1",
     description="A utility to extract vocabulary lists from manga.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
